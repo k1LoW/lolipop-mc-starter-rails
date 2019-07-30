1aed23b789a0d376d658af916456e1ac422d1257
k1LoW <k1lowxb@gmail.com>
2019-07-30 12:16:10 +0900

    bundle gem lolipop-mc-starter

---
+++ lib/lolipop/mc/starter.rb
@@ -0,0 +1,10 @@
+require "lolipop/mc/starter/version"
+
+module Lolipop
+  module Mc
+    module Starter
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/lolipop/mc/starter/version.rb
@@ -0,0 +1,7 @@
+module Lolipop
+  module Mc
+    module Starter
+      VERSION = "0.1.0"
+    end
+  end
+end
